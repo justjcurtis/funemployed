09a8a0965824243be8b401a970bad9380b6af89b
justjcurtis <justjcurtis@gmail.com>
2020-05-11 22:55:29 +0100

    font reduction grammar fix for my

--- js/interviewer.js
+++ js/interviewer.js
@@ -14,6 +14,10 @@ const renderJob = () => {
     let job = getJob();
     let urgency = Math.random()
     let vowels = ["a", "e", "i", "o", "u"]
+    if(job[0] == "my dad" || job[0] == "my mum"){
+        document.getElementById("jobCard").innerHTML = `HELP WANTED<br>I ${urgency<0.2 ? "URGENTLY " : urgency<0.3 ? "DESPERATELY ": ""}NEED ONE OF YOU TO BE ${job[0].toUpperCase()}`
+        return;
+    }
     document.getElementById("jobCard").innerHTML = `HELP WANTED<br>WE ARE IN ${urgency<0.1*1.5 ? "URGENT " : urgency<0.15*1.5 ? "DESPERATE ": urgency< 0.175*1.5 ? "DIRE ": ""}NEED OF ${Math.random<0.2 ? "ONE": Math.random()<0.2 ? "A SINGLE" : vowels.includes(job[0][0]) ? "AN" : "A"}:<br><br>${job[0].toUpperCase()}`;
 }
 
